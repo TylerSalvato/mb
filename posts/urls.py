fff2fb08a902bf48afeeddf6456e78cba8eec296
tylersalvato <tyler.harrissalvato@sdgku.edu>
2023-10-22 13:27:59 -0400

    last push

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path("list/", views.PostListView.as_view(), name="list"),
+    path("", views.PostListView.as_view(), name="list"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
     path("new/", views.PostCreateView.as_view(), name="new"),
 ]
